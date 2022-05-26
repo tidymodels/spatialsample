4352810fc9d37a0584d7bb7fddac7e5d1f8eada3
Mike Mahoney <mike.mahoney.218@gmail.com>
2022-05-26 17:45:49 -0400

    Fix tests

--- tests/testthat/test-autoplot.R
+++ tests/testthat/test-autoplot.R
@@ -1,21 +1,24 @@
 skip_if_not_installed("modeldata")
+skip_if_not_installed("vdiffr")
 
 data(ames, package = "modeldata")
 
 ames_sf <- sf::st_as_sf(ames, coords = c("Longitude", "Latitude"), crs = 4326)
 set.seed(123)
 ames_cluster <- spatial_clustering_cv(ames_sf)
-set.seed(123)
-ames_non_sf <- spatial_clustering_cv(ames, coords = c("Longitude", "Latitude"))
+
 set.seed(123)
 ames_block <- spatial_block_cv(ames_sf)
+
+set.seed(123)
+ames_non_sf <- spatial_clustering_cv(ames, coords = c("Longitude", "Latitude"))
 
 test_that("autoplot is stable", {
 
   p <- autoplot(ames_cluster)
   vdiffr::expect_doppelganger("cluster plots", p)
 
-  p <- autoplot(ames_cluster)
+  p <- autoplot(ames_block)
   vdiffr::expect_doppelganger("block plots", p)
 
   expect_snapshot(
