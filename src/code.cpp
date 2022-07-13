e841553b43e82c44a4af11e400ad4dd070cc669d
Mike Mahoney <mike.mahoney.218@gmail.com>
2022-07-13 09:13:52 -0400

    Suggestions from code review

--- src/code.cpp
+++ src/code.cpp
@@ -7,13 +7,25 @@ cpp11::writable::integers which_within_dist(doubles_matrix<> distmat, doubles id
 
   int n_idx = idx.size();
   int n_matrix = distmat.ncol();
-  std::vector<int> out;
+  std::vector<bool> comparisons(n_matrix);
+  int cur_row;
 
   for (int i = 0; i < n_idx; i++) {
+    cur_row = idx[i] - 1;
     for (int j = 0; j < n_matrix; j++) {
-      if (distmat(idx[i] - 1, j) <= dist) {
-        out.push_back(j + 1);
+      if (distmat(cur_row, j) <= dist) {
+        comparisons[j] = true;
       }
+    }
+  }
+
+  auto n_pos = std::count(comparisons.begin(), comparisons.end(), true);
+  std::vector<int> out(n_pos);
+  int cur_idx = 0;
+  for (int i = 0; i < n_matrix; i++) {
+    if (comparisons[i]) {
+      out[cur_idx] = i + 1;
+      ++cur_idx;
     }
   }
 
